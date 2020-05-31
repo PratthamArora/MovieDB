350d67afd35912f8976d9da64365d7cb78d1437a
PratthamArora <arora.prattham@gmail.com>
2020-05-31 20:41:36 +0530

    added interface for popular movies

--- app/src/main/java/com/pratthamarora/moviedb/data/api/GetMovieDetails.kt
+++ app/src/main/java/com/pratthamarora/moviedb/data/api/GetMovieDetails.kt
@@ -1,12 +1,17 @@
 package com.pratthamarora.moviedb.data.api
 
+import com.oxcoding.moviemvvm.data.vo.MovieResponse
 import com.pratthamarora.moviedb.data.model.MovieDetails
 import io.reactivex.Single
 import retrofit2.http.GET
 import retrofit2.http.Path
+import retrofit2.http.Query
 
 interface GetMovieDetails {
 
     @GET("movie/{movie_id}")
     fun getMovieDetails(@Path("movie_id") id: Int): Single<MovieDetails>
+
+    @GET("movie/popular")
+    fun getPopularMovie(@Query("page") page: Int): Single<MovieResponse>
 }
