a2064a27ed2f1742ee175976992903a47cd98728
PratthamArora <arora.prattham@gmail.com>
2020-05-31 17:24:20 +0530

    added network state class

--- app/src/main/java/com/pratthamarora/moviedb/ui/MainActivity.kt
+++ app/src/main/java/com/pratthamarora/moviedb/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.pratthamarora.moviedb
+package com.pratthamarora.moviedb.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.pratthamarora.moviedb.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
